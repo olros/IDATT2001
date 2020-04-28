5158b341ea2b09fc7e3a0c699905ccf13d45e537
Olaf Rosendahl <olafrosendahl@gmail.com>
2020-04-28 15:24:45 +0200

    Update Client.java

--- Oblig5b_design/Client.java
+++ Oblig5b_design/Client.java
@@ -6,10 +6,11 @@ import java.util.logging.Level;
 public class Client {
 	public static void main(String[] args) {
 		try {
-			ScandinavianWildAnimal ulla = WildAnimalFactorySingleton.newFemaleWolf("Ulla", LocalDate.of(2015, 2, 26), LocalDate.of(2015, 4, 29), "Innhegning 2, Skandinaviske rovdyr", 0);
+			ScandinavianWildAnimal ulla = WildAnimalFactorySingleton.getInstance().newFemaleWolf("Ulla", LocalDate.of(2015, 2, 26), LocalDate.of(2015, 4, 29), "Innhegning 2, Skandinaviske rovdyr", 0);
 			System.out.println(ulla.printInfo());
+			ScandinavianWildAnimal erik = WildAnimalFactorySingleton.getInstance().newMaleBear("Erik", LocalDate.of(2015, 2, 26), LocalDate.of(2015, 4, 29), "Innhegning 2, Skandinaviske rovdyr");
+			System.out.println(erik.printInfo());
 		} catch (Exception e) {
-			e.printStackTrace();
 			ZooLogger.getLogger().log(Level.FINE, e.getMessage());
 		}
 	}
